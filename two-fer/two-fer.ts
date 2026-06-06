c11b65b40e2fbc7bbff6c9e6d219f3ecdb9c0582
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-06 16:45:27 -0300

    solve exercism two fer exercise

---
+++ two-fer/two-fer.ts
@@ -0,0 +1,7 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(receiver? : string) : string {
+  return 'One for ' + (receiver ? receiver : "you") + ', one for me.'
+}
