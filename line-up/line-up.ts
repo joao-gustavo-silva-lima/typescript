b032d377b24ebb68fd9f589f6b7bed7d1553cb41
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-08 15:21:50 -0300

    feature(line-up): solve exercise

--- line-up/line-up.ts
+++ line-up/line-up.ts
@@ -1,3 +1,17 @@
-export function format(name: unknown, number: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+export function format(
+  name: string, 
+  number: number | string
+): string {
+  number = number.toString();
+
+  if(number.endsWith('1') && !number.endsWith("11"))
+    number += "st";
+  else if(number.endsWith('2') && !number.endsWith("12"))
+    number += "nd";
+  else if(number.endsWith('3') && !number.endsWith("13"))
+    number += "rd";
+  else
+    number += "th";
+
+  return `${name}, you are the ${number} customer we serve today. Thank you!`;
 }
