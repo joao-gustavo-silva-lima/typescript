7e0cb4628e03ee5a8a54bf4faa4dd52f2f9171b9
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-10 17:30:12 -0300

    feature(space-age): solve exercise

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,27 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
-}
+const EARTH_ORBITAL_PERIOD_IN_SECS = 31557600;
+
+const PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS = {
+  mars : 1.8808158,
+  earth : 1.000000,
+  venus : 0.61519726,
+  saturn : 29.447498,
+  uranus : 84.016846,
+  mercury : 0.2408467,
+  neptune : 164.79132, 
+  jupiter : 11.862615
+} as const;
+
+const PLANETS = Object.keys(
+  PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS
+) as (keyof typeof PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS)[];
+
+type Planet = typeof PLANETS[number];
+
+
+export var age = (planet: Planet, seconds: number): number => +
+  (seconds / 
+  (
+    PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS[planet] * 
+    EARTH_ORBITAL_PERIOD_IN_SECS
+  ))
+  .toFixed(2);
