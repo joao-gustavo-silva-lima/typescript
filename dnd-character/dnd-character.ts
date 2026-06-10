dbf8f5ac3bec17b66abfee929522a45c2e6ebe5b
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-10 19:50:08 -0300

    feature(dnd-character): solve exercise

--- dnd-character/dnd-character.ts
+++ dnd-character/dnd-character.ts
@@ -1,9 +1,42 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this line and implement the function')
+  public wisdom       : number;
+  public strength     : number;
+  public charisma     : number;
+  public dexterity    : number;
+  public constitution : number;
+  public intelligence : number;
+  
+  public hitpoints    : number;
+
+  constructor() {
+    this.wisdom       = DnDCharacter.generateAbilityScore();
+    this.strength     = DnDCharacter.generateAbilityScore();
+    this.charisma     = DnDCharacter.generateAbilityScore();
+    this.dexterity    = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    
+    this.hitpoints    = 10 + DnDCharacter.getModifierFor(this.constitution);
   }
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this line and implement the function')
+  public static generateAbilityScore(): number {
+    let diceThrowResults : number[] = Array.from(
+      { length : 4 }, () => Dice.throw()
+    )
+    .sort();
+
+    diceThrowResults.shift();
+
+    return diceThrowResults.reduce(
+      (prevValue : number, currValue : number) => prevValue + currValue);
   }
+
+  public static readonly getModifierFor = (abilityValue: number): number =>
+    Math.floor((abilityValue - 10) / 2);
 }
+
+class Dice {
+  public static throw = () : number => 
+    Math.floor(Math.random() * 6) + 1;    
+}
+
