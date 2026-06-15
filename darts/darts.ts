ef0ff553884fd370bcd6e13b7467c15ffcba7e30
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-15 14:32:09 -0300

    refactor(darts): simplify layout
    - Removed getRatiusDistance function declaration.

--- darts/darts.ts
+++ darts/darts.ts
@@ -1,11 +1,8 @@
-const getRatiusDistance = (x : number, y : number) : number =>
-  Math.sqrt(x**2 + y**2);
-
 export function score(x: number, y: number): number {
-  const ratiusDistance = getRatiusDistance(x, y);
+  const ratiusDistance = Math.sqrt(x**2 + y**2);
 
-  if(ratiusDistance <= 1 ) return 10;
-  if(ratiusDistance <= 5 ) return  5;
+  if(ratiusDistance <=  1) return 10;
+  if(ratiusDistance <=  5) return  5;
   if(ratiusDistance <= 10) return  1;
   return 0;
 }
