0a61f0b10fabde1fd7f8df8bf9ce2a20374741e1
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-15 14:06:12 -0300

    feature(darts): solve exercise

--- darts/darts.ts
+++ darts/darts.ts
@@ -1,3 +1,11 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+const getRatiusDistance = (x : number, y : number) : number =>
+  Math.sqrt(x**2 + y**2);
+
+export function score(x: number, y: number): number {
+  const ratiusDistance = getRatiusDistance(x, y);
+
+  if(ratiusDistance <= 1 ) return 10;
+  if(ratiusDistance <= 5 ) return  5;
+  if(ratiusDistance <= 10) return  1;
+  return 0;
 }
