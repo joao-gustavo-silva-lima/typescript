e710fd2254c4b2a025365dcafe67cc93a25d5dc2
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-15 16:20:09 -0300

    feature(bob): solve exercise

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,27 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+const ALPHABET = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+const isYelling = (message : string) : boolean => 
+  message === message.toUpperCase() && ALPHABET.some(l => message.includes(l));
+
+const isSilent  = (message : string) : boolean => 
+  message.trim().length === 0;
+
+const isQuestioning  = (message : string) : boolean => 
+  message.trim().endsWith('?');
+
+export function hey(message: string): string {
+
+  if(isQuestioning(message) && isYelling(message)) 
+    return "Calm down, I know what I'm doing!";
+
+  if(isQuestioning(message)) 
+    return "Sure.";
+
+  if(isYelling(message)) 
+    return "Whoa, chill out!";
+  
+  if(isSilent(message)) 
+    return "Fine. Be that way!";
+  
+  return "Whatever.";
 }
