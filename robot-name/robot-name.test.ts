d45b44a412c4aacec0103b52ce76f66630e38bd2
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-16 20:31:20 -0300

    feature(robot-name): attempt new exercise solution (unconclued)

--- robot-name/robot-name.test.ts
+++ robot-name/robot-name.test.ts
@@ -129,7 +129,7 @@ describe('Robot', () => {
   //
   // This test doesn't run on our online test runner because it will time-out
   // with most implementations. It's up to you to test your solution locally.
-  it.skip('all the names can be generated', () => {
+  it('all the names can be generated', () => {
     const usedNames = new Set()
     usedNames.add(robot.name)
 
--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,8 +1,53 @@
-const ALPHABET = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const ALPHABET = [ ..."ABCDEFGHIJKLMNOPQRSTUVWXYZ" ];
+const DIGITS   = [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  ];
+
+function createObjectFromKeys(keys : string[] | number[], commomValue : number) : Record<string, number> {
+  const obj : Record<string, number> = {};
+
+  for(let key of keys) obj[key] = commomValue;
+
+  return obj;
+} 
+
+export function getRandomObjectKey(obj : Record<string, number>) : string {
+  const keys = Object.keys(obj)
+
+  const output = keys[Math.floor(Math.random() * keys.length)];
+  
+  if(--obj[output] <= 0) delete obj[output];
+
+  return output;
+}
+
+export class NameFactory {
+  public static availableChars = this.getClearAvailableChars();
+
+  public static getClearAvailableChars() {
+    return {
+      alpha   : createObjectFromKeys(ALPHABET, 26E+3),
+      beta    : createObjectFromKeys(ALPHABET,  1000),
+      gamma   : createObjectFromKeys(DIGITS  ,   100),
+      delta   : createObjectFromKeys(DIGITS  ,    10),
+      epsilon : createObjectFromKeys(DIGITS  ,     1),
+    }
+  }
+
+  public static create() : string {
+    return ""
+      + getRandomObjectKey(this.availableChars.alpha  )
+      + getRandomObjectKey(this.availableChars.beta   )
+      + getRandomObjectKey(this.availableChars.gamma  )
+      + getRandomObjectKey(this.availableChars.delta  )
+      + getRandomObjectKey(this.availableChars.epsilon);
+  }
+
+  public static clear() : void { 
+    this.availableChars = this.getClearAvailableChars(); 
+  }
+
+}
 
 export class Robot {
-  private static namesAlreadyTaken : string[] = [];
-
   private _name : string = "";  
 
   public get name(): string {
@@ -11,21 +56,7 @@ export class Robot {
     return this._name;
   }
 
-  public resetName(): void {
-    this._name = ""
-      + ALPHABET[ +(Math.random() * (ALPHABET.length - 1)).toFixed(0) ]
-      + ALPHABET[ +(Math.random() * (ALPHABET.length - 1)).toFixed(0) ]
-      + +(Math.random() * 9).toFixed(0)
-      + +(Math.random() * 9).toFixed(0)
-      + +(Math.random() * 9).toFixed(0)
-    ;
-    
-    if(Robot.namesAlreadyTaken.includes(this._name)) return this.resetName();
+  public resetName() : void { this._name = NameFactory.create(); }
 
-    Robot.namesAlreadyTaken.push(this._name);
-  }
-
-  public static releaseNames(): void {
-    this.namesAlreadyTaken = [];
-  }
+  public static releaseNames = () : void => NameFactory.clear();
 }
