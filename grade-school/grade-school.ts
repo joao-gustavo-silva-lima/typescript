262b973183948f4e8a55f86dc45fc27b20fdafc3
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-16 23:47:17 -0300

    feature(grade-school): solve exercise

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,13 +1,37 @@
 export class GradeSchool {
-  roster() {
-    throw new Error('Remove this line and implement the function')
+  private grades : Record<number, string[]> = {};
+
+  public readonly roster = () : Record<number, string[]> => 
+    Object.keys({...this.grades})
+    .sort()
+    .map(Number)
+    .reduce(
+      (obj, key) => { obj[key] = [...this.grades[key]]; return obj; }, 
+      {} as Record<number, string[]>
+    );
+
+  public readonly grade = (grade : number) : string[] => 
+    this.grades[grade] ? [...this.grades[grade]] : [];
+
+  public add(studentName : string, hostGrade : number) : void {
+    this.dry(studentName);
+
+    this.grades[hostGrade] = hostGrade in this.grades
+      ? [...this.grades[hostGrade], studentName].sort()
+      : [studentName];
   }
 
-  add() {
-    throw new Error('Remove this line and implement the function')
+  private dry(studentName : string) : void {
+    for(let grade of Object.keys(this.grades).map(Number)) {
+      if(!this.grades[grade].includes(studentName)) continue;
+
+      this.remove(studentName, grade);
+    }
   }
 
-  grade() {
-    throw new Error('Remove this line and implement the function')
+  private remove(studentName : string, grade : number) : void {
+    const removalIndex = this.grades[grade].indexOf(studentName);
+
+    this.grades[grade].splice(removalIndex, 1);
   }
 }
