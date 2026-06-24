fab8a989452bea760250d3fad0e4325eef93edc6
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-24 16:53:22 -0300

    feature(secret-handshake): solve exercise

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,3 +1,26 @@
-export function commands() {
-  throw new Error('Remove this line and implement the function')
+const COMMANDS = ["jump", "close your eyes", "double blink", "wink"] as const;
+
+type Command = typeof COMMANDS[number];
+
+export function commands(entryNumber : number) : Command[] {
+  if(entryNumber < 1 || entryNumber > 31) return [];
+
+  const binnaryDigits = [...entryNumber.toString(2).padStart(5, '0')];
+  const hasReverseCommand = binnaryDigits.shift() === '1';
+  const output : Command[] = [];
+  
+  for(let i = 0 ; i < binnaryDigits.length ; i++) {
+    if(binnaryDigits[i] === '0') continue;
+
+    const command = COMMANDS[i];
+
+    if(hasReverseCommand) {
+      output.push(command);
+    }
+    else {
+      output.unshift(command);
+    }
+  }
+
+  return output;
 }
