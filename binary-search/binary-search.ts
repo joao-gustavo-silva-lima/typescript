d3e595e12fadbf47ccf0ddf981a9b4ff1c8b7eeb
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-25 16:36:57 -0300

    feature(binary-search): solve exercise

--- binary-search/binary-search.ts
+++ binary-search/binary-search.ts
@@ -1,3 +1,20 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this line and implement the function')
+export function find(haystack: number[], needle: number): number {
+  var low  = 0;
+  var high = haystack.length - 1;
+  
+  while(low <= high) {
+    const mid  = Math.floor((high + low) / 2);
+
+    if(haystack[mid] === needle) {
+      return mid;
+    }  
+    if(needle < haystack[mid]) {
+      high = mid - 1;
+    }
+    else {
+      low = mid + 1;
+    }
+  }
+
+  throw new Error("Value not in array");
 }
