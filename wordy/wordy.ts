9844934ce23654f5ea8a80a1d759f0e056545729
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-06-30 21:01:00 -0300

    feature(wordy.ts): unfinish exercise solution

--- wordy/wordy.ts
+++ wordy/wordy.ts
@@ -1,3 +1,28 @@
-export const answer = () => {
-  throw new Error('Remove this line and implement the function')
+const OPERATOR_EXPRESSIONS = new Map([
+  ["plus"         , '+'],
+  ["minus"        , '-'],
+  ["divided by"   , '/'],
+  ["multiplied by", '*'],
+]);
+
+const MATH_EXPRESSIONS_REGEX = /(?:-?\d+|plus|minus|divided by|multiplied by)/g;
+
+export const answer = (question: string): number => {
+  const operationalTerms = question.match(MATH_EXPRESSIONS_REGEX);
+  const isMathQuestion = /^What is/.test(question);
+
+  if(!operationalTerms) {
+    throw new Error(isMathQuestion ? "Syntax error" : "Unknown operation");
+  }
+
+  let operationString = "";
+
+  for(const term of operationalTerms) {
+    operationString = OPERATOR_EXPRESSIONS.has(term)
+      ? operationString + OPERATOR_EXPRESSIONS.get(term)! 
+      : `(${operationString}(${term}))`;
+  }
+
+  try   { return eval(operationString) as number; }
+  catch {    throw new Error("Syntax error");     }
 }
