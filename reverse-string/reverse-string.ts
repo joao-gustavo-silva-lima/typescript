e41c3cf72d2472425206fd31eb62c0c1d9f3c6ce
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-03 17:50:55 -0300

    refactor(reverse-string.ts): use native array methods
    - Replaced the manual backward for-loop iteration with native split, reverse, and join methods.

--- reverse-string/reverse-string.ts
+++ reverse-string/reverse-string.ts
@@ -1,7 +1,3 @@
 export function reverse(entryString: string) {
-  let reversedString = "";
-  for(let i = entryString.length - 1 ; i >= 0 ; i--) {
-    reversedString += entryString[i];
-  }
-  return reversedString;
+  return entryString.split('').reverse().join('');
 }
