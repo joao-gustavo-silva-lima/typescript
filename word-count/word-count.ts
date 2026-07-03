f3bed010541fbadd8434bd4616a58341676b04f5
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-03 14:57:18 -0300

    feature(word-count.ts): solve exercise

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,3 +1,12 @@
-export function count() {
-  throw new Error('Remove this line and implement the function')
+export function count(sentence: string) {
+  const counterMap = new Map<string, number>();
+  const words = sentence.toLowerCase().match(/[a-z\d]+(?:'[a-z\d]+)*/gi);
+
+  if(!words) return counterMap;
+
+  for(const word of words) {
+    counterMap.set(word, (counterMap.get(word) || 0) + 1);
+  }
+
+  return counterMap;
 }
