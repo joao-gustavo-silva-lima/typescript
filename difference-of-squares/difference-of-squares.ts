dc2ad78198930cd3f7a305cdcc0e371547ff9dba
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-03 15:30:20 -0300

    feature(difference-of-squares.ts): solve exercise

--- difference-of-squares/difference-of-squares.ts
+++ difference-of-squares/difference-of-squares.ts
@@ -1,17 +1,27 @@
 export class Squares {
-  constructor(count: unknown) {
-    throw new Error('Remove this line and implement the function')
+  public sumOfSquares: number;
+  public  squareOfSum: number;
+  public   difference: number;
+
+  constructor(count: number) {
+    this.sumOfSquares = Squares.calculateSumOfSquares(count);
+    this.squareOfSum  =  Squares.calculateSquareOfSum(count);
+    this.difference   = this.squareOfSum - this.sumOfSquares;
   }
 
-  get sumOfSquares(): unknown {
-    throw new Error('Remove this line and implement the function')
+  private static calculateSumOfSquares(count: number): number {
+    let result = 0;
+    for(let i = 1 ; i <= count ; i++) {
+      result += i ** 2;
+    }
+    return result; 
   }
 
-  get squareOfSum(): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  get difference(): unknown {
-    throw new Error('Remove this line and implement the function')
+  private static calculateSquareOfSum(count: number): number {
+    let result = 0;
+    for(let i = 1 ; i <= count ; i++) {
+      result += i;
+    }
+    return result ** 2;
   }
 }
