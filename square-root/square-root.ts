aa36680d801a863516fd44f52c526c1c851e1c55
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-03 17:05:07 -0300

    feature(square-root.ts): solve exercise

--- square-root/square-root.ts
+++ square-root/square-root.ts
@@ -1,3 +1,17 @@
-export function squareRoot(radicand: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+const applyHeronFormula = (nearNumber: number, radicand: number): number =>
+  (nearNumber + radicand/nearNumber) / 2
+
+export function squareRoot(radicand: number): number {
+  let nearNumber = 0;
+  while(++nearNumber ** 2 < radicand) {}
+
+  let result: number = nearNumber;
+  
+  while(true) {
+    const heronResult = applyHeronFormula(nearNumber, radicand);
+    if(nearNumber === heronResult) break;
+    result = heronResult;
+  }
+
+  return result;
 }
