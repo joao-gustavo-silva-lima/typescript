03e684d7ffa522f372929006cf26347b3c7834e5
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-03 17:42:29 -0300

    feature(reverse-string.ts): solve exercise

--- reverse-string/reverse-string.ts
+++ reverse-string/reverse-string.ts
@@ -1,3 +1,7 @@
-export function reverse(/* Parameters go here */) {
-  throw new Error('Remove this line and implement the function')
+export function reverse(entryString: string) {
+  let reversedString = "";
+  for(let i = entryString.length - 1 ; i >= 0 ; i--) {
+    reversedString += entryString[i];
+  }
+  return reversedString;
 }
