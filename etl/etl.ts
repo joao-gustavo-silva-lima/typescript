2facf47baec6847cc963b6941e8456d1872fdc69
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-04 14:32:52 -0300

    feature(etl.ts): solve exercise

--- etl/etl.ts
+++ etl/etl.ts
@@ -1,3 +1,12 @@
-export function transform(/* Parameters go here */) {
-  throw new Error('Remove this line and implement the function')
+type NumberStringPair = { [key: number]: string[] };
+type LetterNumberPair = { [key: string]: number };
+
+export function transform(old: NumberStringPair): LetterNumberPair {
+  const output: LetterNumberPair = {};
+
+  for(const [point, letters] of Object.entries(old)) {
+    letters.forEach((letter) => output[letter.toLowerCase()] = Number(point));
+  }
+
+  return output;
 }
