be80857640fe4edcbe4b6a76a0b72ed04f1c1c70
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-05 10:52:09 -0300

    feature(hamming.ts): solve exercise

--- hamming/hamming.ts
+++ hamming/hamming.ts
@@ -1,3 +1,13 @@
-export function compute(left: unknown, right: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+export function compute(sourceDNA: string, replicatedDNA: string): number {
+  if(replicatedDNA.length !== sourceDNA.length) {
+    throw "DNA strands must be of equal length.";
+  }
+  
+  let hammingDistance = 0;
+
+  for(let i = 0 ; i < sourceDNA.length ; i++) {
+    if(replicatedDNA[i] !== sourceDNA[i]) hammingDistance++;
+  }
+
+  return hammingDistance;
 }
