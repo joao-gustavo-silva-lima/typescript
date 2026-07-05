add1446efa71b112db06c21542ab487b7fe117f4
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-05 11:15:19 -0300

    feature(nucleotide-count.ts): solve exercise locally

--- nucleotide-count/nucleotide-count.ts
+++ nucleotide-count/nucleotide-count.ts
@@ -1,3 +1,15 @@
-export function nucleotideCounts(/* Parameters go here */) {
-  throw new Error('Remove this line and implement the function')
+export function nucleotideCounts(DNA: string): Record<string, number> {
+  const count: Record<string, number> = {
+    A: 0,
+    C: 0,
+    G: 0,
+    T: 0
+  };
+
+  for(const nucleotide of DNA) {
+    if(!/[ACGT]/.test(nucleotide)) throw "Invalid nucleotide in strand";
+    count[nucleotide]++;
+  }
+
+  return count;
 }
