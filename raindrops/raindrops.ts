8e89c7daf45aa4aac3fab0908da53d9ce533c4cf
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-05 10:38:56 -0300

    feature(raindrops.ts): solve exercise

--- raindrops/raindrops.ts
+++ raindrops/raindrops.ts
@@ -1,3 +1,7 @@
-export function convert() {
-  throw new Error('Remove this line and implement the function')
+export function convert(entryNumber: number): string {
+  return [
+    entryNumber % 3 === 0 ? "Pling" : '',
+    entryNumber % 5 === 0 ? "Plang" : '',
+    entryNumber % 7 === 0 ? "Plong" : ''
+  ].join('') || entryNumber.toString();
 }
