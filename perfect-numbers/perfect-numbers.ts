2655b4f15c532e4dcb0230193a6ffd76f16bc458
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-07 17:06:55 -0300

    feature(perfect-numbers.ts): solve exercise

--- perfect-numbers/perfect-numbers.ts
+++ perfect-numbers/perfect-numbers.ts
@@ -1,3 +1,19 @@
-export function classify() {
-  throw new Error('Remove this line and implement the function')
+type Classification = "perfect" | "abundant" | "deficient";
+
+export function classify(inputNumber: number): Classification {
+  if (inputNumber <= 0 || !Number.isInteger(inputNumber)) {
+    throw "Classification is only possible for natural numbers.";
+  }
+
+  let aliquotSum = 0;
+
+  for (let n = 1; n < inputNumber; n++) {
+    if (inputNumber % n !== 0) continue;
+
+    aliquotSum += n;
+  }
+
+  if (aliquotSum > inputNumber) return "abundant";
+  if (aliquotSum < inputNumber) return "deficient";
+  return "perfect";
 }
