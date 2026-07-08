93c787f084f2a4738bea4b0de48b604ac287aaaa
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-08 17:08:30 -0300

    feature(sum-of-multiples.ts): solve exercise

--- sum-of-multiples/sum-of-multiples.ts
+++ sum-of-multiples/sum-of-multiples.ts
@@ -1,3 +1,19 @@
-export function sum() {
-  throw new Error('Remove this line and implement the function')
+export function sum(magicalItemValues: number[], playerLevel: number): number {
+  let energyPoint = 0;
+
+  const alreadyTakenMultiples = new Set<number>();
+
+  for (const value of magicalItemValues) {
+    if (value <= 0) continue;
+
+    for (let multiple = value; multiple < playerLevel; multiple += value) {
+      if (alreadyTakenMultiples.has(multiple)) {
+        continue;
+      }
+      alreadyTakenMultiples.add(multiple);
+      energyPoint += multiple;
+    }
+  }
+
+  return energyPoint;
 }
