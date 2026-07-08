46164516061adcc827867db8c200b7ff9a78fc9c
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-08 11:44:01 -0300

    feature(complex-numbers.ts): solve exercise

--- complex-numbers/complex-numbers.ts
+++ complex-numbers/complex-numbers.ts
@@ -1,41 +1,54 @@
 export class ComplexNumber {
-  constructor(real: unknown, imaginary: unknown) {
-    throw new Error('Remove this line and implement the function')
+  public readonly real: number;
+  public readonly imag: number;
+  public readonly abs: number;
+
+  public readonly expReal: number;
+  public readonly expImag: number;
+
+  constructor(real: number, imag: number) {
+    this.real = real;
+    this.imag = imag;
+    this.abs = Math.sqrt(this.real ** 2 + this.imag ** 2);
+
+    const purelyReal = Math.exp(this.real);
+    this.expReal = purelyReal * Math.cos(this.imag);
+    this.expImag = purelyReal * Math.sin(this.imag);
   }
 
-  public get real(): number {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public get imag(): number {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public add(other: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public sub(other: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public div(other: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public mul(other: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public get abs(): unknown {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public get conj(): unknown {
-    throw new Error('Remove this line and implement the function')
+  public get conj(): ComplexNumber {
+    return new ComplexNumber(this.real, -this.imag || 0);
   }
 
   public get exp(): ComplexNumber {
-    throw new Error('Remove this line and implement the function')
+    return new ComplexNumber(this.expReal, this.expImag);
+  }
+
+  public add(other: ComplexNumber): ComplexNumber {
+    return new ComplexNumber(this.real + other.real, this.imag + other.imag);
+  }
+
+  public sub(other: ComplexNumber): ComplexNumber {
+    return new ComplexNumber(this.real - other.real, this.imag - other.imag);
+  }
+
+  public div(other: ComplexNumber): ComplexNumber {
+    const denominator = other.real ** 2 + other.imag ** 2;
+
+    if (denominator === 0) {
+      throw new Error("Cannot divide a complex number by zero.");
+    }
+
+    return new ComplexNumber(
+      (this.real * other.real + this.imag * other.imag) / denominator,
+      (this.imag * other.real - this.real * other.imag) / denominator
+    );
+  }
+
+  public mul(other: ComplexNumber): ComplexNumber {
+    return new ComplexNumber(
+      this.real * other.real - this.imag * other.imag,
+      this.imag * other.real + this.real * other.imag
+    );
   }
 }
