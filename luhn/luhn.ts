6aa126807cdac324900b59e05d6906b8950c1291
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-08 12:26:51 -0300

    feature(luhn.ts): solve exercise

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -1,3 +1,27 @@
-export function valid(digitString: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+export function valid(digitString: string): boolean {
+  const cleanString = digitString.replace(/\s+/g, "");
+
+  if (!/^[\d]{2,}$/.test(cleanString)) {
+    return false;
+  }
+
+  let sumForValidation = 0;
+  let index = 1;
+
+  while (cleanString.length - index >= 0) {
+    let digit = Number(cleanString[cleanString.length - index]);
+
+    if (index % 2 === 0) {
+      digit = digit * 2 - (digit * 2 > 9 ? 9 : 0);
+    }
+
+    sumForValidation += digit;
+    index++;
+  }
+
+  if (sumForValidation % 10 !== 0) {
+    return false;
+  }
+
+  return true;
 }
