3919f60c33154675be41bd9199c1a22e0abc8bf0
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-10 14:24:34 -0300

    feature(isogram.ts): solve exercise

--- isogram/isogram.ts
+++ isogram/isogram.ts
@@ -1,3 +1,15 @@
-export function isIsogram(/* parameters go here */): unknown {
-  throw new Error('Remove this line and implement the function')
+export function isIsogram(word: string): unknown {
+  const characters = new Set<string>();
+
+  for (const character of word.toLowerCase()) {
+    if (/[\s-]/.test(character)) {
+      continue;
+    }
+    if (characters.has(character)) {
+      return false;
+    }
+    characters.add(character);
+  }
+
+  return true;
 }
