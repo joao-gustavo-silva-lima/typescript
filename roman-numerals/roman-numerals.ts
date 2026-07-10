2fe830b9011f1bbf0debad1b3cbfd16d2c7918b1
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-10 15:29:51 -0300

    feature(roman-numerals.ts): solve exercise

--- roman-numerals/roman-numerals.ts
+++ roman-numerals/roman-numerals.ts
@@ -1,3 +1,38 @@
-export const toRoman = () => {
-  throw new Error('Remove this line and implement the function')
+export const toRoman = (inputNumber: number): string => {
+  if (inputNumber > 3999) {
+    throw new Error("Choose a number in the following interval: [1, 3999]");
+  }
+
+  let output = "";
+
+  const thousands = Math.floor(inputNumber / 1000) % 10;
+  const hundreds = Math.floor(inputNumber / 100) % 10;
+  const tens = Math.floor(inputNumber / 10) % 10;
+  const unit = inputNumber % 10;
+
+  output += "M".repeat(thousands);
+  output += convertNumeralComponentToRoman(hundreds, "C", "D", "CM");
+  output += convertNumeralComponentToRoman(tens, "X", "L", "XC");
+  output += convertNumeralComponentToRoman(unit, "I", "V", "IX");
+
+  return output;
+};
+
+function convertNumeralComponentToRoman(
+  component: number,
+  romamIncrementComponent: string,
+  romanMiddleContainer: string,
+  romanEdgeCaseComponent: string
+) {
+  if (component <= 3) {
+    return romamIncrementComponent.repeat(component);
+  }
+  if (component <= 5) {
+    return romamIncrementComponent.repeat(5 - component) + romanMiddleContainer;
+  }
+  if (component <= 8) {
+    return romanMiddleContainer + romamIncrementComponent.repeat(component - 5);
+  }
+
+  return romanEdgeCaseComponent;
 }
