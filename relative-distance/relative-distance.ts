2212972a3b77a802d3c9c3ebf5b89e89834288e2
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-10 11:29:33 -0300

    feature(relative-distance): solve exercise

--- relative-distance/relative-distance.ts
+++ relative-distance/relative-distance.ts
@@ -1,7 +1,52 @@
 export function degreesOfSeparation(
-  familyTree: unknown,
-  personA: unknown,
-  personB: unknown
-): unknown {
-  throw new Error('Remove this line and implement the function')
+  familyTree: { [key: string]: string[] },
+  personA: string,
+  personB: string
+): number {
+  const neighborhood = new Map<string, Set<string>>();
+
+  for (const [person, children] of Object.entries(familyTree)) {
+    const personNeighbors = neighborhood.get(person) ?? new Set();
+
+    neighborhood.set(person, personNeighbors);
+
+    for (const child of children) {
+      personNeighbors.add(child);
+
+      const childNeighbors = neighborhood.get(child) ?? new Set();
+
+      neighborhood.set(child, childNeighbors);
+      childNeighbors.add(person);
+
+      for (const sibling of children) {
+        if (sibling !== child) {
+          childNeighbors.add(sibling);
+        }
+      }
+    }
+  }
+
+  if (!neighborhood.has(personA) || !neighborhood.has(personB)) {
+    return -1;
+  }
+
+  const parsingQueue: [string, number][] = [[personA, 0]];
+  const alreadyParsed = new Set([personA]);
+
+  while (parsingQueue.length > 0) {
+    const [person, degree] = parsingQueue.shift()!;
+
+    if (person === personB) {
+      return degree;
+    }
+
+    for (const neighbor of neighborhood.get(person)!) {
+      if (!alreadyParsed.has(neighbor)) {
+        alreadyParsed.add(neighbor);
+        parsingQueue.push([neighbor, degree + 1]);
+      }
+    }
+  }
+
+  return -1;
 }
