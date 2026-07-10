0936386e97529403e390f81b72f652edffa29523
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-10 14:20:03 -0300

    chore(isogram.ts): setup exercise locally

---
+++ isogram/isogram.test.ts
@@ -0,0 +1,74 @@
+import { describe, it, expect } from "@jest/globals";
+import { isIsogram } from "./isogram.ts";
+
+describe("Check if the given string is an isogram", () => {
+  it("empty string", () => {
+    const expected = true;
+    expect(isIsogram("")).toEqual(expected);
+  });
+
+  it("isogram with only lower case characters", () => {
+    const expected = true;
+    expect(isIsogram("isogram")).toEqual(expected);
+  });
+
+  it("word with one duplicated character", () => {
+    const expected = false;
+    expect(isIsogram("eleven")).toEqual(expected);
+  });
+
+  it("word with one duplicated character from the end of the alphabet", () => {
+    const expected = false;
+    expect(isIsogram("zzyzx")).toEqual(expected);
+  });
+
+  it("longest reported english isogram", () => {
+    const expected = true;
+    expect(isIsogram("subdermatoglyphic")).toEqual(expected);
+  });
+
+  it("word with duplicated character in mixed case", () => {
+    const expected = false;
+    expect(isIsogram("Alphabet")).toEqual(expected);
+  });
+
+  it("word with duplicated character in mixed case, lowercase first", () => {
+    const expected = false;
+    expect(isIsogram("alphAbet")).toEqual(expected);
+  });
+
+  it("hypothetical isogrammic word with hyphen", () => {
+    const expected = true;
+    expect(isIsogram("thumbscrew-japingly")).toEqual(expected);
+  });
+
+  it("hypothetical word with duplicated character following hyphen", () => {
+    const expected = false;
+    expect(isIsogram("thumbscrew-jappingly")).toEqual(expected);
+  });
+
+  it("isogram with duplicated hyphen", () => {
+    const expected = true;
+    expect(isIsogram("six-year-old")).toEqual(expected);
+  });
+
+  it("made-up name that is an isogram", () => {
+    const expected = true;
+    expect(isIsogram("Emily Jung Schwartzkopf")).toEqual(expected);
+  });
+
+  it("duplicated character in the middle", () => {
+    const expected = false;
+    expect(isIsogram("accentor")).toEqual(expected);
+  });
+
+  it("same first and last characters", () => {
+    const expected = false;
+    expect(isIsogram("angola")).toEqual(expected);
+  });
+
+  it("word with duplicated character and with two hyphens", () => {
+    const expected = false;
+    expect(isIsogram("up-to-date")).toEqual(expected);
+  });
+});
