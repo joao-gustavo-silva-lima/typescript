964391144ed86528caa7703a648d96997135cb34
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-12 17:49:29 -0300

    feature(series.ts): solve exercise

--- series/series.ts
+++ series/series.ts
@@ -1,9 +1,41 @@
 export class Series {
-  constructor(series: unknown) {
-    throw new Error('Remove this line and implement the function')
+  private series: string;
+
+  constructor(series: string) {
+    this.series = series;
   }
 
-  slices(sliceLength: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
+  slices(sliceLength: number): number[][] {
+    if (this.series.length === 0) {
+      throw new Error("series cannot be empty");
+    }
+    if (sliceLength < 0) {
+      throw new Error("slice length cannot be negative");
+    }
+    if (sliceLength === 0) {
+      throw new Error("slice length cannot be zero");
+    }
+    if (sliceLength > this.series.length) {
+      throw new Error("slice length cannot be greater than series length");
+    }
+
+    const slices: number[][] = [];
+    const sliceQuantity = this.series.length - sliceLength + 1;
+
+    let slicingOriginIndex = 0;
+    while (slicingOriginIndex < sliceQuantity) {
+      const slice: number[] = [];
+
+      for (let i = 0; i < sliceLength; i++) {
+        const digit = this.series[slicingOriginIndex + i];
+
+        slice.push(Number(digit));
+      }
+
+      slices.push(slice);
+      slicingOriginIndex++;
+    }
+
+    return slices;
   }
 }
