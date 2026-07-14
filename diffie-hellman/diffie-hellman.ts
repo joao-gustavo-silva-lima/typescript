1c425bf20d29fd8833240ce027fca98f4df8a385
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-07-14 18:13:58 -0300

    chore(diffie-hellman): setup exercise locally

---
+++ diffie-hellman/diffie-hellman.ts
@@ -0,0 +1,13 @@
+export class DiffieHellman {
+  constructor(p: unknown, g: unknown) {
+    throw new Error('Remove this line and implement the function')
+  }
+
+  public getPublicKey(privateKey: unknown): unknown {
+    throw new Error('Remove this line and implement the function')
+  }
+
+  public getSecret(theirPublicKey: unknown, myPrivateKey: unknown): unknown {
+    throw new Error('Remove this line and implement the function')
+  }
+}
